b37ab0a1389c1ad9c261f447abb8c8cd2595858a
Keegan Lawley <keeganlk.7@gmail.com>
2018-10-09 16:55:12 -0500

    custom cursors

--- draw.js
+++ draw.js
@@ -40,11 +40,11 @@ function clearCanvas() {
 }
 
 function colorMenu() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("palette-show");
 }
 
 function settingsMenu() {
-  document.getElementById("settingsDropdown").classList.toggle("show");
+  document.getElementById("settingsDropdown").classList.toggle("settings-show");
 }
 
 var palette = document.querySelector("#palette");
@@ -61,8 +61,12 @@ var white = document.querySelector("#white");
 var black = document.querySelector("#black");
 
 
-palette.onclick = colorMenu;
-settings.onclick = settingsMenu;
+palette.onclick = () => {
+  canvas.classList.remove('eraser');
+  canvas.classList.add('pen');
+  colorMenu();
+  ctx.lineWidth = 10;
+};
 
 red.onclick = () => {
   ctx.strokeStyle = "#FF4444";
@@ -90,7 +94,37 @@ black.onclick = () => {
 };
 
 eraser.onclick = () => {
-  // ctx.lineWidth = 100;
+  canvas.classList.remove('pen');
+  canvas.classList.add('eraser');
+  ctx.lineWidth = 50;
   ctx.strokeStyle = "#FFFEE1";
 
 };
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
